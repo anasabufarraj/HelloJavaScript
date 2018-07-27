457de38ee9f3ee7c6b4148962a441cd8b3ae0998
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-07-27 10:23:44 +0300

    Add .map files.

---
+++ js/console.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function output(value) {
+    console.log(value);
+}
+exports.output = output;
+//# sourceMappingURL=console.js.map
---
+++ js/functional.js
@@ -0,0 +1,11 @@
+var line = console.log;
+function output(value) {
+    console.log(value);
+}
+line('Hellos');
+function foo(admin) {
+    if (admin === undefined) {
+        console.log(admin);
+    }
+}
+//# sourceMappingURL=functional.js.map
