3cc97a694d154f3dfdcc4dbecd86616e1c3b697e
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-02-22 13:21:33 +0200

    backup files.

--- source/scripts/playground.js
+++ source/scripts/playground.js
@@ -1,1 +1,4 @@
+jQuery(() => {
+  jQuery('footer small').text('Ready to Manipulate!');
+});
 //# sourceMappingURL=playground.js.map
--- source/scripts/playground.ts
+++ source/scripts/playground.ts
@@ -1,3 +1,1 @@
-// (c) Copyright 2018, Anas Abu Farraj.
-// TypeScript Playground
-// 29 Jul 2018
+// (c) Copyright 2018, Anas Abu Farraj.// TypeScript Playground// 29 Jul 2018jQuery(() => {  // change the footer message at the bottom of the page  jQuery('footer small').text('Ready to Manipulate!');  // jQuery('#rect')  //   // animate width to 200px on mouseover  //   .on('mouseover', () => {  //     jQuery('#rect').animate({ width: 200 }, 300);  //   })  //  //   // animate width to 100px on mouseout  //   .on('mouseout', () => {  //     jQuery('#rect').animate({ width: 100, height: 100 }, 300);  //   })  //  //   // increase height by 100px on click  //   .on('click', () => {  //     jQuery('#rect').animate({ height: '+=100' }, 'slow');  //   });});
